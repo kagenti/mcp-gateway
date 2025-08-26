84b4e3a93f376f3ea0be4a6201191dfee9270f54
craig <cbrookes@redhat.com>
2025-08-26 08:08:53 +0100

    adds a stop channel for graceful shutdown and a config reload
    Signed-off-by: craig <cbrookes@redhat.com>

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,25 @@
-FROM golang:1.22 AS builder
+FROM golang:1.23 AS builder
 
-WORKDIR /internal
+WORKDIR /workspace
 
-COPY go.* .
 
+COPY cmd/mcp-broker-router/main.go cmd/mcp-broker-router/main.go
+COPY internal/ internal/
+
+COPY go.mod go.mod
+COPY go.sum go.sum
 RUN go mod download
 
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o mcp_gateway cmd/mcp-broker-router/main.go
+
+FROM alpine:latest
+
+RUN apk --no-cache add ca-certificates
+
+WORKDIR /app
+
+COPY --from=builder /workspace/mcp_gateway .
+
+RUN chmod +x mcp_gateway
+
+CMD ["./mcp_gateway", "--mcp-gateway-config=/config/config.yaml"]
--- cmd/mcp-broker-router/main.go
+++ cmd/mcp-broker-router/main.go
@@ -2,52 +2,125 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
 	"time"
 
 	extProcV3 "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
+	"github.com/fsnotify/fsnotify"
+	config "github.com/kagenti/mcp-gateway/internal/config"
 	mcpRouter "github.com/kagenti/mcp-gateway/internal/mcp-router"
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
 
-func getEnv(key, fallback string) string {
-	if v, ok := os.LookupEnv(key); ok {
-		return v
-	}
-	return fallback
-}
+var (
+	mcpConfig config.MCPServersConfig
+	mutex     sync.RWMutex
+)
 
 func main() {
-	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		_, _ = fmt.Fprint(w, "Hello, World!")
+	var (
+		mcpRouterAddrFlag string
+		mcpBrokerAddrFlag string
+		mcpConfigFile     string
+	)
+	flag.StringVar(&mcpRouterAddrFlag, "mcp-router-address", "0.0.0.0:50051", "The address for mcp router")
+	flag.StringVar(&mcpBrokerAddrFlag, "mcp-broker-address", "0.0.0.0:8080", "The address for mcp broker")
+	flag.StringVar(&mcpConfigFile, "mcp-gateway-config", "./config/mcp-system/config.yaml", "where to locate the mcp server config")
+	flag.Parse()
+	LoadConfig(mcpConfigFile)
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		log.Println("mcp servers config changed ", in.Name)
+		mutex.Lock()
+		defer mutex.Unlock()
+		LoadConfig(mcpConfigFile)
 	})
-	httpSrv := &http.Server{
-		Addr:         getEnv("HTTP_ADDR", ":8080"),
-		Handler:      mux,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-	go func() {
-		log.Printf("[http] listening on %s", httpSrv.Addr)
-		if err := httpSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("[http] %v", err)
-		}
-	}()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	brokerServer := setUpBroker(mcpBrokerAddrFlag)
+	routerServer := setUpRouter()
 
-	grpcAddr := getEnv("SERVER_ADDRESS", "0.0.0.0:9002")
+	grpcAddr := fmt.Sprintf(mcpRouterAddrFlag)
 	lis, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
 		log.Fatalf("[grpc] listen error: %v", err)
 	}
-	grpcSrv := grpc.NewServer()
-	extProcV3.RegisterExternalProcessorServer(grpcSrv, &mcpRouter.ExtProcServer{})
 
-	log.Printf("[grpc] listening on %s", grpcAddr)
-	log.Fatal(grpcSrv.Serve(lis))
+	go func() {
+		log.Printf("[grpc] starting MCP Router listening on %s", grpcAddr)
+		log.Fatal(routerServer.Serve(lis))
+	}()
+
+	go func() {
+		log.Printf("[http] starting MCP Broker listening on %s", brokerServer.Addr)
+		if err := brokerServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("[http] %v", err)
+		}
+	}()
+
+	<-stop
+	// handle shutdown
+	log.Printf("shutting down MCP Broker and MCP Router")
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+	if err := brokerServer.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
+	}
+	routerServer.GracefulStop()
+}
+
+func setUpBroker(address string) *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		_, _ = fmt.Fprint(w, "Hello, World!")
+	})
+	httpSrv := &http.Server{
+		Addr:         address,
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	return httpSrv
+}
+
+func setUpRouter() *grpc.Server {
+	grpcSrv := grpc.NewServer()
+	extProcV3.RegisterExternalProcessorServer(grpcSrv, &mcpRouter.ExtProcServer{
+		MCPConfig: &mcpConfig,
+	})
+	return grpcSrv
+}
+
+// config
+
+func LoadConfig(path string) {
+	viper.SetConfigFile(path)
+	log.Println("loading congfig from path", viper.ConfigFileUsed())
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
+	err = viper.UnmarshalKey("servers", &mcpConfig.Servers)
+	if err != nil {
+		log.Fatalf("Unable to decode server config into struct: %s", err)
+	}
+
+	log.Println("config loaded ")
+
+	for _, s := range mcpConfig.Servers {
+		fmt.Println("server name", s.Name)
+		fmt.Println("server host", s.Hostname)
+		fmt.Println("server prefix", s.ToolPrefix)
+		fmt.Println("server backend", s.URL)
+	}
 }
