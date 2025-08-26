91176113a0a7da89af70bcdcb937f0d42d95b7ee
Ed Snible <snible@us.ibm.com>
2025-08-25 20:47:36 -0400

    Broker tests that really run
    Signed-off-by: Ed Snible <snible@us.ibm.com>

---
+++ internal/tests/server2/server2_test.go
@@ -0,0 +1,91 @@
+package server2
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHello(t *testing.T) {
+	res, err := helloHandler(context.Background(), mcp.CallToolRequest{})
+	require.NoError(t, err)
+	require.True(t, res.IsError)
+
+	res, err = helloHandler(context.Background(), mcp.CallToolRequest{
+		Params: mcp.CallToolParams{
+			Arguments: map[string]any{
+				"name": "Fred",
+			},
+		},
+	})
+	require.NoError(t, err)
+	require.False(t, res.IsError)
+	require.IsType(t, mcp.TextContent{}, res.Content[0])
+	require.Equal(t, "Hello, Fred!", res.Content[0].(mcp.TextContent).Text)
+}
+
+func TestTime(t *testing.T) {
+	res, err := timeHandler(context.Background(), mcp.CallToolRequest{})
+	require.NoError(t, err)
+	require.False(t, res.IsError)
+	require.IsType(t, mcp.TextContent{}, res.Content[0])
+}
+
+func TestHeaders(t *testing.T) {
+	res, err := headersToolHandler(context.Background(), mcp.CallToolRequest{})
+	require.NoError(t, err)
+	require.False(t, res.IsError)
+	require.Len(t, res.Content, 0)
+
+	res, err = headersToolHandler(context.Background(), mcp.CallToolRequest{
+		Header: http.Header{
+			"Authorization": []string{"bearer 1234"},
+		},
+	})
+	require.NoError(t, err)
+	require.False(t, res.IsError)
+	require.Len(t, res.Content, 1)
+	require.IsType(t, &mcp.TextContent{}, res.Content[0])
+	require.Equal(t, "Authorization: [bearer 1234]", res.Content[0].(*mcp.TextContent).Text)
+}
+
+func TestSlow(t *testing.T) {
+	res, err := slowHandler(context.Background(), mcp.CallToolRequest{})
+	require.NoError(t, err)
+	require.True(t, res.IsError)
+
+	res, err = slowHandler(context.Background(), mcp.CallToolRequest{
+		Params: mcp.CallToolParams{
+			Arguments: map[string]any{
+				"seconds": "0",
+			},
+		},
+	})
+	require.NoError(t, err)
+	require.False(t, res.IsError)
+	require.IsType(t, mcp.TextContent{}, res.Content[0])
+}
+
+func TestAuth1234(t *testing.T) {
+	_, err := auth1234ToolHandler(context.Background(), mcp.CallToolRequest{})
+	require.Error(t, err)
+
+	res, err := auth1234ToolHandler(context.Background(), mcp.CallToolRequest{
+		Header: http.Header{
+			"Authorization": []string{"bearer 1234"},
+		},
+	})
+	require.NoError(t, err)
+	require.False(t, res.IsError)
+	require.IsType(t, mcp.TextContent{}, res.Content[0])
+}
+
+func TestRunStreamableServer(t *testing.T) {
+	startFunc, shutdownFunc, err := RunServer("http", "8085")
+	require.NoError(t, err)
+	require.NotNil(t, startFunc)
+	require.NotNil(t, shutdownFunc)
+}
