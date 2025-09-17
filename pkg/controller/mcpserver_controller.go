0a66eb5322d3e40e485332dfc9da926b12c9a2ba
Jason Madigan <jason@jasonmadigan.com>
2025-09-17 15:56:35 +0100

    updating MCPServer to add a new path field. Added a new test server, with a custom path
    Signed-off-by: Jason Madigan <jason@jasonmadigan.com>

--- pkg/controller/mcpserver_controller.go
+++ pkg/controller/mcpserver_controller.go
@@ -27,6 +27,8 @@ const (
 	ConfigNamespace = "mcp-system"
 	// ConfigName is the name of the config
 	ConfigName = "mcp-gateway-config"
+	// DefaultMCPPath is the default path for MCP endpoints
+	DefaultMCPPath = "/mcp"
 )
 
 // generateCredentialEnvVar generates an environment variable name for credentials
@@ -422,7 +424,13 @@ func (r *MCPReconciler) discoverServersFromHTTPRoutes(
 		}
 	}
 
-	endpoint := fmt.Sprintf("%s://%s/mcp", protocol, nameAndEndpoint)
+	// use custom path if specified, otherwise default to DefaultMCPPath
+	path := mcpServer.Spec.Path
+	if path == "" {
+		path = DefaultMCPPath
+	}
+
+	endpoint := fmt.Sprintf("%s://%s%s", protocol, nameAndEndpoint, path)
 
 	// external services need actual hostname for routing
 	routingHostname := hostname
